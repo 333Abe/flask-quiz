c6cf6c8f15929c06efdebf290225e3c00be54850
Adrian <adrianbennett333@gmail.com>
2024-01-23 16:40:00 +0000

    added login/logout and start-quiz

--- quiz.py
+++ quiz.py
@@ -12,18 +12,28 @@ app.config['SECRET_KEY'] = 'my_secret_key'
 # initialise db with SQLAlchemy init_app function
 db.init_app(app)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
 
     form = StartQuiz()
 
     if form.validate_on_submit():
         session["user"] = form.name.data
-    return render_template("index.html")
+
+    return render_template("index.html", form=form)
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for('home'))
+
+@app.route('/start-quiz')
+def start_quiz():
+    return redirect(url_for('question'))
 
 @app.route("/question")
 def question():
-    return render_template("question.html")
+    return render_template('question.html')
 
 @app.route("/answer")
 def answer():
